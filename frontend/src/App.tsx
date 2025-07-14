003a26da967a20cddd4cac8f9dfc4200d4b270b0
pdlsagar677 <sagarpoudel677@gmail.com>
2025-07-14 12:08:27 +0545

    pages combination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { Toaster } from 'sonner';
- import Home from './components/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
+ import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
