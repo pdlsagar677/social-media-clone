990fb3b595c55c2c9fbebc1f12093925bb52b5da
pdlsagar677 <sagarpoudel677@gmail.com>
2025-09-19 11:23:46 +0545

    edit profile successfully completed ad changes in routing side

--- backend/src/routes/user-route.ts
+++ backend/src/routes/user-route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register,login ,logout, getProfile, getSuggestedUsers, followOrUnfollow} from '../controllers/user-controller';
+import { register,login ,logout, getProfile, getSuggestedUsers, followOrUnfollow, editProfile} from '../controllers/user-controller';
 import isAuthenticated from '../middlewares/isAuthenticated';
 import { uploadProfilePicture } from '../middlewares/multer';
 
@@ -8,7 +8,7 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/:id/profile', isAuthenticated , getProfile);
-router.post('/profile/edit', isAuthenticated , uploadProfilePicture);
+router.post('/profile/edit', isAuthenticated , uploadProfilePicture, editProfile);
 router.get('/suggested', isAuthenticated , getSuggestedUsers);
 router.post('/followorunfollow/:id', isAuthenticated , followOrUnfollow);
 
